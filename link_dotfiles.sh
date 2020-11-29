8b0a718fdd0132f2366c66b184fb2b111f36feb7
Stephan Tarulli <srt@tinychameleon.com>
2020-11-28 20:24:56 -0800

    Exclude certain git files from dotfile linking

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd -P)"
-/usr/bin/find "$SCRIPT_DIR" -depth 1 -name '.*' -and -not -name '.git' -exec ln -sft $HOME {} +
+/usr/bin/find "$SCRIPT_DIR" -depth 1 -name '.*' \
+	-and -not \( \
+		-name '.git' \
+		-or -name '.gitignore' \
+		-or -name '.gitmodules' \
+		\) \
+	-exec ln -sft $HOME {} +
